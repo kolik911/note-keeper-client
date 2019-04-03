fa192ef903716907352bddf60e967c2770518b5f
Mykola Kachmaryk <kolia.kachmaruk@gmail.com>
2019-04-03 13:54:22 +0300

    fix TodoService

--- src/TodoService.js
+++ src/TodoService.js
@@ -3,22 +3,6 @@
 const url = "http://localhost:5000/api/posts/";
 
 class PostService {
-  // static getPosts() {
-  //   return new Promise(async (resolve, reject) => {
-  //     try {
-  //       const res = await axios.get(url);
-  //       const data = res.data;
-  //       resolve(
-  //         data.map(post => ({
-  //           ...post,
-  //           createdAt: new Date(post.createdAt)
-  //         }))
-  //       );
-  //     } catch (err) {
-  //       reject(err);
-  //     }
-  //   });
-  // }
   static async getTodos() {
     const api_call = await fetch(url);
     const data = await api_call.json();
@@ -32,7 +16,6 @@ class PostService {
       cache: "no-cache",
       headers: {
         "Content-Type": "application/json"
-        // "Content-Type": "application/x-www-form-urlencoded",
       },
       body: JSON.stringify({ text })
     }).then(response => response.json());
@@ -49,20 +32,6 @@ class PostService {
       body: JSON.stringify(todo)
     }).then(response => response.json());
   }
-
-  // static editTodo(id, text) {
-  //   return fetch(`${url}${id}`, {
-  //     method: "PUT",
-  //     mode: "cors",
-  //     cache: "no-cache",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //       // "Content-Type": "application/x-www-form-urlencoded",
-  //     },
-  //     body: JSON.stringify({ text })
-  //   }).then(response => response.json());
-  //   // console.log(id, data);
-  // }
 
   static deleteTodo(id) {
     return fetch(`${url}${id}`, {
